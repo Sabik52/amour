6c3d18f20f17f74772683e48dbb0636ef8edc7b0
Sabik52 <rahman52neajur@gmail.com>
2022-05-17 20:32:39 +0600

    about added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            
+        <div className='about-section'>
+            <h1>About Us</h1>
+            <h3>Address: Bondor, Chattogram</h3>
+            <h4>Contact: 01840441285</h4>
+            <h5>Email: amour.perfume@gmail.com </h5>
         </div>
     );
 };
